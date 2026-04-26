dc98e93ef71c6ec9b87a9a82ec45fd419cfe415b
Muhammed Shibili <muhammedshibili688@gmail.com>
2026-04-26 22:17:08 +0530

    chore: update project configuration, DVC pipeline, and environment setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Muhammed Shibili",
+    author_email="muhammedshibili688@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
